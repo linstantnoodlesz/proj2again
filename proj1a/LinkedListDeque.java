f4d42a5f0d9650111953f3c92a4d7a35daf08136
Gilbert Lin <gilbertlin@Hellls-Kitchen.local>
2017-02-03 11:53:50 -0800

    finished coding, running tests

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -4,20 +4,31 @@
 
 import javax.lang.model.element.VariableElement;
 
-/** creates LinkedListDeque, which should be a double ended queue formed with LinkedLists */
+/** creates LinkedListDeque, which is a double ended queue formed with LinkedLists */
 public class LinkedListDeque <Var> {
     private class ItemNode {
         public Var item;
         public ItemNode next;
+
+        public ItemNode() {
+            item = null;
+            next = null;
+        }
 
         public ItemNode(Var i, ItemNode n) {
             item = i;
             next = n;
         }
     }
+
     /** creates sentinel node; the first item is found at sentinel.next */
-    private ItemNode sentinel;
+    private class SentinelNode {
+        public ItemNode front;
+        public ItemNode back;
+
+    }
     private int size;
+    private SentinelNode sentinel;
 
     /** returns the variable size (incremented across multiple methods) */
     public int size() {
@@ -26,20 +37,100 @@ public class LinkedListDeque <Var> {
 
     /** creates empty LinkedListDeque */
     public LinkedListDeque() {
-        sentinel = new ItemNode(null, null);
+        sentinel.front = null;
+        sentinel.back = null;
         size = 0;
     }
 
-    /** Constructs the LinkedListDeque class */
+    /** Constructs a LinkedListDeque */
     public LinkedListDeque(Var x) {
-        sentinel = new ItemNode(null,null);
-        sentinel.next = new ItemNode(x, null);
+        sentinel.front = new ItemNode(x, null);
+        sentinel.back = sentinel.front;
         size = 1;
     }
 
     /** adds some x to the front of the list */
     public void addFirst(Var x) {
-        sentinel.next = new ItemNode(x, sentinel.next);
+        ItemNode newFront = new ItemNode(x, sentinel.front);
+        sentinel.front = newFront;
         size += 1;
     }
+    /** adds some x to the back of the list */
+    public void addLast(Var x) {
+        ItemNode newLast = new ItemNode(x, null);
+        sentinel.back = newLast;
+        size += 1;
+
+        }
+    /** returns whether a deque is empty or not */
+    public boolean isEmpty() {
+        if (size()== 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Prints the items in the Deque from first to last, separated by a space. */
+    public void printDeque() {
+        System.out.print(sentinel.front + " ");
+        ItemNode p = sentinel.front;
+        while (p.next != null) {
+            System.out.print(p.next + " ");
+            p = p.next;
+        }
+    }
+
+    /** Removes and returns the item at the front of the Deque. If no such item exists, returns null. */
+     public Var removeFirst() {
+        if (sentinel.front.item != null) {
+            Var removeditem = sentinel.front.item;
+            sentinel.front = sentinel.front.next;
+            size -= 1;
+            return removeditem;
+        }
+        return null;
+    }
+    /** Removes and returns the item at the back of the Deque. If no such item exists, returns null. */
+    public Var removeLast() {
+         if (sentinel.back.item != null) {
+             Var removeditem = sentinel.back.item;
+             ItemNode p = sentinel.front;
+             while (p.next != sentinel.back) {
+                 p = p.next;
+             }
+             sentinel.back = p;
+             size -= 1;
+             return removeditem;
+         }
+         return null;
+    }
+
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no such item exists, returns null. */
+    public Var get(int index) {
+        if (index > size) {
+            return null;
+        }
+        int k = 0;
+        ItemNode p = sentinel.front;
+        while (k != index) {
+            p = p.next;
+            k++;
+        }
+        return p.item;
+    }
+
+    public Var getRecursive(int index) {
+        ItemNode p = sentinel.front;
+        ItemNode k = sentinel.front;
+        if (index > size) {
+            return null;
+        }
+        if (index == 0) {
+            return p.item;
+        }
+        k = p.next;
+        return getRecursive(index - 1);
+    }
+
 }
+
