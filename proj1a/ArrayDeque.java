3aa60766f10a6b3b8ec9e99bfefa86fe38323ed2
Gilbert Lin <gilbertlin@Hellls-Kitchen.local>
2017-02-03 23:40:47 -0800

    cleaned up

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -2,16 +2,6 @@
  * Created by gilbertlin on 2/3/17.
  */
 public class ArrayDeque<Var> {
-    /**
-     * DONE public void addFirst(Item): Adds an item to the front of the Deque.
-     * DONE public void addLast(Item): Adds an item to the back of the Deque.
-     * DONE public boolean isEmpty(): Returns true if deque is empty, false otherwise.
-     * DONE public int size(): Returns the number of items in the Deque.
-     * DONE public void printDeque(): Prints the items in the Deque from first to last, separated by a space.
-     * DONE public Item removeFirst(): Removes and returns the item at the front of the Deque. If no such item exists, returns null.
-     * DONE public Item removeLast(): Removes and returns the item at the back of the Deque. If no such item exists, returns null.
-     * DONE public Item get(int index): Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no such item exists, returns null.
-     */
     private Var[] items;
 
     private int size;
