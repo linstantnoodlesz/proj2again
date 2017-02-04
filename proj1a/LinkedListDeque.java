519f4b62ef1e141e5d587a7eba725a7cdb955c6f
Gilbert Lin <gilbertlin@Hellls-Kitchen.local>
2017-02-04 11:30:05 -0800

    adding to empty list

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -52,12 +52,18 @@ public class LinkedListDeque<Item> {
 
     /** adds some x to the front of the list */
     public void addFirst(Item x) {
+        if (isEmpty()) {
+            sentinel.back = sentinel.front;
+        }
         ItemNode newFront = new ItemNode(x, sentinel.front, null);
         sentinel.front = newFront;
         size += 1;
     }
     /** adds some x to the back of the list */
     public void addLast(Item x) {
+        if (isEmpty()) {
+            sentinel.front = sentinel.back;
+        }
         ItemNode newLast = new ItemNode(x, null, sentinel.back);
         sentinel.back = newLast;
         size += 1;
