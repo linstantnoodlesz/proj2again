397592e7a32bf643d41b3e2ea55153cd7f3180bc
Gilbert Lin <gilbertlin@Hellls-Kitchen.local>
2017-02-08 02:30:30 -0800

    test written?

--- proj1b/TestArrayDeque1B.java
+++ proj1b/TestArrayDeque1B.java
@@ -4,7 +4,28 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-@Test
-public void tester() {
-    assertEquals()
+public class TestArrayDeque1B {
+    @Test
+    public void tester() {
+        int input = StdRandom.uniform(10);
+        StudentArrayDeque student = new StudentArrayDeque();
+        ArrayDequeSolution soln = new ArrayDequeSolution();
+        boolean value_agree = true;
+        while (value_agree) {
+            student.addFirst(input);
+            student.addFirst(input);
+            input = StdRandom.uniform(10);
+            soln.addFirst(input);
+            soln.addFirst(input);
+            input = StdRandom.uniform(10);
+            student.addLast(input);
+            soln.addLast(input);
+            student.removeLast();
+            soln.removeLast();
+            student.removeFirst();
+            soln.removeFirst();
+            assertEquals(student.get(0), soln.get(0));
+
+        }
+    }
 }
