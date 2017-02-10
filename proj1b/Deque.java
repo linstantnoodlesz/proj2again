913892c0f87825dc30d1a6ceead47d50817e6a01
Gilbert Lin <gilbertlin@Hellls-Kitchen.local>
2017-02-09 22:52:57 -0800

    finished methods + interface

---
+++ proj1b/Deque.java
@@ -0,0 +1,14 @@
+/**
+ * Created by gilbertlin on 2/9/17.
+ */
+public interface Deque <Item> {
+    public void addFirst(Item x);
+    public void addLast(Item x);
+    public boolean isEmpty();
+    public int size();
+    public void printDeque();
+    public Item removeFirst();
+    public Item removeLast();
+    public Item get(int index);
+    public Item getRecursive(int index);
+}
