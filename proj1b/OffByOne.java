cb296b499916065dfbff4c5c86f422fe9127a256
Gilbert Lin <gilbertlin@Hellls-Kitchen.local>
2017-02-10 01:28:29 -0800

    finished equal chars method

---
+++ proj1b/OffByOne.java
@@ -0,0 +1,12 @@
+/**
+ * Created by gilbertlin on 2/10/17.
+ */
+public class OffByOne implements CharacterComparator {
+    @Override
+    public boolean equalChars(char x, char y) {
+        if (Math.abs(x-y) == 1) {
+            return true;
+        }
+        return false;
+    }
+}
