a3e7e3214e9ab124bb8309e5441af24eaef0dc9d
Gilbert Lin <gilbertlin@airbears2-10-142-37-130.airbears2.1918.berkeley.edu>
2017-02-10 14:09:06 -0800

    finished coding offbyN

---
+++ proj1b/OffByN.java
@@ -0,0 +1,15 @@
+/**
+ * Created by gilbertlin on 2/10/17.
+ */
+public class OffByN implements CharacterComparator {
+    public int N;
+    public OffByN(int N) {
+        N = N;
+    }
+    public boolean equalChars(char x, char y) {
+        if (Math.abs(x-y) == N) {
+            return true;
+        }
+        return false;
+    }
+}
