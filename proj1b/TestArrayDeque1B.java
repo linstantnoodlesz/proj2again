fbf99b529e359dbb9ca6eb4491b20e81d11ed980
Gilbert Lin <gilbertlin@Hellls-Kitchen.local>
2017-02-10 20:49:24 -0800

    tested for resizing

--- proj1b/TestArrayDeque1B.java
+++ proj1b/TestArrayDeque1B.java
@@ -3,35 +3,43 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestArrayDeque1B {
+
     @Test
     public void tester() {
         int input = StdRandom.uniform(10);
         StudentArrayDeque student = new StudentArrayDeque();
         ArrayDequeSolution soln = new ArrayDequeSolution();
-        while (true) {
+
+        String string = "";
+        int sizeTo = 4;
+        while (sizeTo > 0) {
             student.addFirst(input);
             soln.addFirst(input);
-            String string = "\naddFirst(" + input + ")";
+            string += "\naddFirst(" + input + ")";
 
             input = StdRandom.uniform(10);
             student.addFirst(input);
             soln.addFirst(input);
-            string = string + "\naddFirst(" + input + ")";
+            string += "\naddFirst(" + input + ")";
 
             input = StdRandom.uniform(10);
             student.addLast(input);
             soln.addLast(input);
-            string = string + "\naddLast(" + input + ")";
+            string += "\naddLast(" + input + ")";
 
             input = StdRandom.uniform(10);
             student.addLast(input);
             soln.addLast(input);
-            string = string + "\naddLast(" + input + ")";
+            string += "\naddLast(" + input + ")";
+            sizeTo--;
+        }
 
-            assertEquals(string + "\nremoveLast()", soln.removeLast(), student.removeLast());
-            string += "\nremoveLast()";
+        while (soln.size() > 0) {
             assertEquals(string + "\nremoveFirst()", soln.removeFirst(), student.removeFirst());
             string += "\nremoveFirst()";
+            assertEquals(string + "\nremoveLast()", soln.removeLast(), student.removeLast());
+            string += "\nremoveLast()";
         }
     }
+
 }
