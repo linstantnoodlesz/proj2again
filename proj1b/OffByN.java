fcfad73f0aeb72bbdd82236665ea68b85b33d2ab
Gilbert Lin <gilbertlin@Hellls-Kitchen.local>
2017-02-10 20:54:42 -0800

    fixed offbyN

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -2,13 +2,13 @@
  * Created by gilbertlin on 2/10/17.
  */
 public class OffByN implements CharacterComparator {
-    private int N;
+    private int x;
 
     public OffByN(int N) {
-        N = N;
+        x = N;
     }
     public boolean equalChars(char x, char y) {
-        if (Math.abs(x - y) == N) {
+        if (Math.abs(x - y) == x) {
             return true;
         }
         return false;
